8a06190e716413030c24ae570e866a31af05d50a
DashingQi <m13478493231@163.com>
2021-03-01 14:38:57 +0800

    [update]

--- app/src/main/java/com/dashingqi/composeproject/MainActivity.kt
+++ app/src/main/java/com/dashingqi/composeproject/MainActivity.kt
@@ -40,73 +40,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    @Preview
-    @Composable
-    fun artistCard(name: String) {
-        val padding = 16.dp
-        //图片
-        val headerImg = imageResource(id = R.drawable.ic)
-        //头像图片的修饰符
-        val headerModifier = Modifier
-            // 设置图片的高
-            .preferredHeight(48.dp)
-            //设置图片的宽
-            .preferredWidth(48.dp)
-            //设置图片的圆角
-            .clip(RoundedCornerShape(24.dp))
 
-        //banner的图片资源
-        val bannerImg = imageResource(id = R.drawable.banner)
-        //banner的修饰符
-        val bannerModifier = Modifier
-            .preferredHeight(128.dp)
-            //让宽度填充布局
-            .fillMaxWidth()
-
-        Column(Modifier.padding(padding).fillMaxWidth()) {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-
-                Image(
-                    bitmap = headerImg,
-                    modifier = headerModifier,
-                    contentScale = ContentScale.Crop
-                )
-                //加入间距
-                Spacer(Modifier.preferredSize(8.dp))
-
-                Column(horizontalAlignment = Alignment.Start) {
-                    Text(name, color = Color.Blue, fontSize = 15.sp)
-                    Spacer(Modifier.preferredSize(4.dp))
-                    Text("3 minutes ago", color = Color.Black, fontSize = 12.sp)
-                }
-            }
-            Spacer(Modifier.preferredSize(padding))
-
-            // 卡片的布局
-            Card(elevation = 4.dp) {
-                Image(
-                    modifier = bannerModifier,
-                    bitmap = bannerImg,
-                    contentScale = ContentScale.Crop
-                )
-            }
-        }
-    }
-
-    @Composable
-    fun Feed(feedItems: List<String>) {
-//        ScrollableRow(Modifier.wrapContentHeight().fillMaxWidth()) {
-//            feedItems.forEach {
-//                artistCard(it)
-//            }
-//        }
-        Surface(Modifier.fillMaxSize()) {
-            LazyColumnFor(feedItems) { item ->
-                artistCard(item)
-
-            }
-        }
-    }
 
 
     /**
@@ -198,5 +132,73 @@ class MainActivity : AppCompatActivity() {
                     state.snackbarHostState.showSnackbar(message = "点击了悬浮按钮")
                 }
             })
+    }
+
+    @Preview
+    @Composable
+    fun artistCard(name: String) {
+        val padding = 16.dp
+        //图片
+        val headerImg = imageResource(id = R.drawable.ic)
+        //头像图片的修饰符
+        val headerModifier = Modifier
+            // 设置图片的高
+            .preferredHeight(48.dp)
+            //设置图片的宽
+            .preferredWidth(48.dp)
+            //设置图片的圆角
+            .clip(RoundedCornerShape(24.dp))
+
+        //banner的图片资源
+        val bannerImg = imageResource(id = R.drawable.banner)
+        //banner的修饰符
+        val bannerModifier = Modifier
+            .preferredHeight(128.dp)
+            //让宽度填充布局
+            .fillMaxWidth()
+
+        Column(Modifier.padding(padding).fillMaxWidth()) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+
+                Image(
+                    bitmap = headerImg,
+                    modifier = headerModifier,
+                    contentScale = ContentScale.Crop
+                )
+                //加入间距
+                Spacer(Modifier.preferredSize(8.dp))
+
+                Column(horizontalAlignment = Alignment.Start) {
+                    Text(name, color = Color.Blue, fontSize = 15.sp)
+                    Spacer(Modifier.preferredSize(4.dp))
+                    Text("3 minutes ago", color = Color.Black, fontSize = 12.sp)
+                }
+            }
+            Spacer(Modifier.preferredSize(padding))
+
+            // 卡片的布局
+            Card(elevation = 4.dp) {
+                Image(
+                    modifier = bannerModifier,
+                    bitmap = bannerImg,
+                    contentScale = ContentScale.Crop
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun Feed(feedItems: List<String>) {
+//        ScrollableRow(Modifier.wrapContentHeight().fillMaxWidth()) {
+//            feedItems.forEach {
+//                artistCard(it)
+//            }
+//        }
+        Surface(Modifier.fillMaxSize()) {
+            LazyColumnFor(feedItems) { item ->
+                artistCard(item)
+
+            }
+        }
     }
 }
